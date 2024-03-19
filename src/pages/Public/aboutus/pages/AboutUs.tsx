a7ae0cb38061c19e61f67b0a269e90013305bf04
HAms25 <hilari.martinez@tecsup.edu.pe>
2024-03-19 08:42:04 -0500

    :sparkles: Add Members Team Section

--- src/pages/Public/aboutus/pages/AboutUs.tsx
+++ src/pages/Public/aboutus/pages/AboutUs.tsx
@@ -6,9 +6,9 @@ const AboutUs = () => {
   return (
     <>
     <figure className="flex w-full h-full">
-        <img src="public/aboutus.svg" alt="" className="w-full max-h-[60vh] object-cover"/>
+        <img src="/aboutus.svg" alt="" className="w-full max-h-[60vh] object-cover"/>
     </figure>
-    <SectionBlock title="Conoce más de Nosotros" className="p-1 flex items-center py-20">
+    <SectionBlock title="Conoce más de Nosotros" className="p-1 flex items-center md:py-20 py-5">
       <div className="flex flex-col md:flex-row w-full justify-center items-center md:w-[95%] gap-8 xl:max-w-[60%]">
         <Card className="p-4 pt-12 border-dashed border-2 border-foreground flex flex-col items-center space-y-5 md:w-full justify-center">
           <div>
@@ -35,7 +35,30 @@ const AboutUs = () => {
       </div>
     </SectionBlock>
     <SectionBlock title="Nuestro Team" className="p-1 flex items-center">
-
+      <div className="flex flex-col md:flex-row justify-center items-center gap-8 w-full">
+    <Card className="p-1 flex flex-col items-center justify-center space-y-5 md:">
+        <figure className="p-5 w-[14.5rem] md:w-[15rem]">
+          <img src="/userExample.svg" alt="" className="rounded-full"/>
+        </figure>
+        <CardTitle className="font-bold text-[1.3rem]">
+          Hilari Martinez
+        </CardTitle>
+        <CardContent className="text-justify">
+          SafeHaven
+        </CardContent>
+      </Card>
+      <Card className="p-1 flex flex-col items-center justify-center space-y-5">
+        <figure className="p-5 w-[14.5rem] md:w-[15rem]">
+          <img src="/userExample.svg" alt="" className="rounded-full"/>
+        </figure>
+        <CardTitle className="font-bold text-[1.3rem]">
+          Hilari Martinez
+        </CardTitle>
+        <CardContent className="text-justify">
+          SafeHaven
+        </CardContent>
+      </Card>
+      </div>
     </SectionBlock>
     </>
   )
