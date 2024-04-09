93e572f8b1a93b6dd4f1f2e7b4b872300ac34833
HAms25 <hilari.martinez@tecsup.edu.pe>
2024-04-09 18:45:54 -0500

    :hammer: Work in Progress

--- src/lib/validators/requestScheme.ts
+++ src/lib/validators/requestScheme.ts
@@ -1,27 +1,29 @@
 import { z } from "zod";
 
-const requestSchema = z.object({
-  username: z
+const contactSchema = z.object({
+  name: z
     .string()
     .min(2, {
-      message: "Username must be at least 2 characters.",
+      message: "Name must be at least 2 characters.",
     })
     .max(30, {
-      message: "Username must not be longer than 30 characters.",
-    }),
-  email: z
-    .string({
-      required_error: "Please select an email to display.",
+      message: "Name must not be longer than 30 characters.",
     })
-    .email(),
-  bio: z.string().max(160).min(4),
-  urls: z
-    .array(
-      z.object({
-        value: z.string().url({ message: "Please enter a valid URL." }),
-      })
-    )
-    .optional(),
+    .trim(),
+  email: z
+    .string()
+    .min(1, { message: "Se requiere un correo electronico" })
+    .email("Este no es un correo electrónico válido")
+    .trim(),
+  message: z
+    .string()
+    .max(160)
+    .min(4)
+    .trim(),
+  phone: z
+    .string()
+    .min(9, { message: "Ingrese un número de teléfono" })
+    .max(9, { message: "Número de teléfono inválido" }),
 });
 
-export default requestSchema 
+export default contactSchema;
--- src/pages/Public/aboutus/pages/AboutUs.tsx
+++ src/pages/Public/aboutus/pages/AboutUs.tsx
@@ -1,68 +1,101 @@
-import { SectionBlock } from "@/common/components"
-import { Card, CardContent, CardTitle } from "@/components/ui/Card"
-import { Rocket } from "lucide-react"
+import { SectionBlock } from "@/common/components";
+import { Card, CardContent, CardTitle } from "@/components/ui/Card";
+import { Rocket, Star } from "lucide-react";
+import { Contact } from "../../request/pages/Request";
 
 const AboutUs = () => {
   return (
     <>
-    <figure className="flex w-full h-full">
-        <img src="/aboutus.svg" alt="" className="w-full max-h-[60vh] object-cover"/>
-    </figure>
-    <SectionBlock title="Conoce más de Nosotros" className="p-1 flex items-center md:py-20 py-5">
-      <div className="flex flex-col md:flex-row w-full justify-center items-center md:w-[95%] gap-8 xl:max-w-[60%]">
-        <Card className="p-4 pt-12 border-dashed border-2 border-foreground flex flex-col items-center space-y-5 md:w-full justify-center">
-          <div>
-          <Rocket className="size-12"/>
+      <figure className="flex w-full h-full aspect-auto">
+        <img
+          src="/aboutus.svg"
+          alt=""
+          className="w-full max-h-[60vh] object-cover "
+        />
+      </figure>
+      <SectionBlock
+        title="Conoce más de Nosotros"
+        className="p-1 flex items-center md:pt-20 py-5 md:gap-20"
+      >
+        <div className="flex flex-col w-full md:w-[95%] gap-16 xl:max-w-[60%]">
+          <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
+          <Card className="p-4 pt-8 border-dashed border-2 border-foreground/50 flex flex-col items-center space-y-5 md:w-full justify-center">
+            <div>
+              <Rocket className="size-12" />
+            </div>
+            <CardTitle className="font-bold text-[1.5rem]">
+              Nuestra misión
+            </CardTitle>
+            <CardContent className="text-justify">
+              Nuestra misión es proporcionar soluciones personalizadas y seguras
+              de mantenimiento del hogar que permitan a nuestros clientes
+              proteger, mejorar y disfrutar de sus espacios vitales. Nos
+              esforzamos por transformar la forma en que se abordan las
+              necesidades de mantenimiento del hogar, utilizando procesos
+              eficientes para brindar resultados excepcionales a nuestros
+              clientes.
+            </CardContent>
+          </Card>
+          <figure>
+            <img src="https://res.cloudinary.com/dovqxavyy/image/upload/v1712633510/Abput_us-transformed_jxcfg5.png" alt="" className="rounded-xl w-[20rem] md:w-[40rem] md:h-[31rem]" />
+          </figure>
           </div>
-          <CardTitle className="font-bold text-[1.5rem]">
-            Nuestra misión
-          </CardTitle>
-          <CardContent className="text-justify">
-          Lo más importante es conectar a las personas con los mejores servicios locales, impulsar la economía local y crear oportunidades para los emprendedores.
-          </CardContent>
-        </Card>
-        <Card className="p-4 pt-12 border-dashed border-2 border-foreground flex flex-col items-center space-y-5 md:w-full justify-center">
-          <div>
-          <Rocket className="size-12"/>
+          <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
+            <figure className="order-last md:order-none">
+              <img src="https://res.cloudinary.com/dovqxavyy/image/upload/v1712633510/aboutus2-transformed_uzn3ti.png" alt="" className="rounded-xl w-[20rem] md:w-[40rem] md:h-[32rem]"/>
+            </figure>
+          <Card className="p-4 pt-12 border-dashed border-2 border-foreground/50 flex flex-col items-center space-y-5 md:w-full justify-center">
+            <div>
+              <Star className="size-12" />
+            </div>
+            <CardTitle className="font-bold text-[1.5rem]">
+              Lo más Importante
+            </CardTitle>
+            <CardContent className="text-justify">
+              Lo que nos mueve es la pasión por crear hogares seguros y
+              confortables. Nos impulsa la visión de ser líderes en la industria
+              del mantenimiento del hogar, ofreciendo soluciones confiables y
+              accesibles que satisfagan las necesidades únicas de cada hogar.
+              Nos imaginamos un futuro donde cada persona pueda disfrutar de la
+              tranquilidad de saber que su hogar está en buenas manos.
+            </CardContent>
+          </Card>
           </div>
-          <CardTitle className="font-bold text-[1.5rem]">
-            Lo más Importante
-          </CardTitle>
-          <CardContent className="text-justify">
-          Lo más importante es conectar a las personas con los mejores servicios locales, impulsar la economía local y crear oportunidades para los emprendedores.
-          </CardContent>
-        </Card>
-      </div>
-    </SectionBlock>
-    <SectionBlock title="Nuestro Team" className="p-1 flex items-center">
-      <div className="flex flex-col md:flex-row justify-center items-center gap-8 w-full">
-    <Card className="p-1 flex flex-col items-center justify-center space-y-5 md:">
-        <figure className="p-5 w-[14.5rem] md:w-[15rem]">
-          <img src="/userExample.svg" alt="" className="rounded-full"/>
-        </figure>
-        <CardTitle className="font-bold text-[1.3rem]">
-          Hilari Martinez
-        </CardTitle>
-        <CardContent className="text-justify">
-          SafeHaven
-        </CardContent>
-      </Card>
-      <Card className="p-1 flex flex-col items-center justify-center space-y-5">
-        <figure className="p-5 w-[14.5rem] md:w-[15rem]">
-          <img src="/userExample.svg" alt="" className="rounded-full"/>
-        </figure>
-        <CardTitle className="font-bold text-[1.3rem]">
-          Hilari Martinez
-        </CardTitle>
-        <CardContent className="text-justify">
-          SafeHaven
-        </CardContent>
-      </Card>
-      </div>
-    </SectionBlock>
+        </div>
+      </SectionBlock>
+      <SectionBlock
+        title="Nuestro Team"
+        className=" flex items-center md:gap-20"
+      >
+        <div className="flex flex-col md:flex-row justify-center items-center gap-8 w-full">
+          <Card className="p-1 flex flex-col items-center justify-center space-y-5 md:">
+            <figure className="p-5 w-[14.5rem] md:w-[15rem]">
+              <img src="/userExample.svg" alt="" className="rounded-full" />
+            </figure>
+            <CardTitle className="font-bold text-[1.3rem]">
+              Hilari Martinez
+            </CardTitle>
+            <CardContent className="text-justify">SafeHaven</CardContent>
+          </Card>
+          <Card className="p-1 flex flex-col items-center justify-center space-y-5">
+            <figure className="p-5 w-[14.5rem] md:w-[15rem]">
+              <img src="/userExample.svg" alt="" className="rounded-full" />
+            </figure>
+            <CardTitle className="font-bold text-[1.3rem]">
+              Hilari Martinez
+            </CardTitle>
+            <CardContent className="text-justify">SafeHaven</CardContent>
+          </Card>
+        </div>
+      </SectionBlock>
+      <SectionBlock
+        title="Contáctanos"
+        className="flex items-center md:gap-20 p-1"
+      >
+        <Contact />
+      </SectionBlock>
     </>
-  )
-}
+  );
+};
 
-export default AboutUs
-
+export default AboutUs;
